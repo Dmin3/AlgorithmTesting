d9ab4e0b9dbcb36e29e99a63dc76b80b8c686400
Dmin3 <ekdrnzld@gmail.com>
2022-01-29 16:50:20 +0900

    1/29

--- src/Number1001.java
+++ src/Number1001.java
@@ -3,22 +3,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Number10430 {
+public class Number1001 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str, " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
 
-
-        System.out.println((a + b) % c);
-        System.out.println((a % c + b % c) % c);
-        System.out.println((a * b) % c);
-        System.out.println((a % c * b % c) % c);
-
-
+        System.out.println(a - b);
     }
 }
