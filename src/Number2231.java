9c034dfb15843f56cf979ab6c06c611b201f428a
Dmin3 <ekdrnzld@gmail.com>
2022-02-03 15:11:53 +0900

    2/3

---
+++ src/Number2231.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Number2231 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int val = 0;
+        for (int i = 0; i < N; i++) {
+            int num = i;
+            int sum =0;
+
+            while (num != 0){
+                sum += num % 10;
+                num /= 10;
+            }
+
+            if (sum + i == N){
+                val = i;
+                break;
+            }
+        }
+
+        System.out.println(val);
+
+
+
+    }
+}
