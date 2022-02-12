4cfd6632447aca0904206efa459a5c3d5535d4a8
Dmin3 <ekdrnzld@gmail.com>
2022-02-12 19:29:14 +0900

    연습문제 풀이

--- src/Practice.java
+++ src/Practice.java
@@ -2,15 +2,17 @@ import javax.swing.event.InternalFrameListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Practice {
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       int a =20;
+       int b =30;
 
-        String s = br.readLine();
+        int max = Math.max(20, 30);
+        System.out.println(max);
 
-        System.out.println(s);
 
     }
 }
