772192fb28734694d0b47c0b6462a7bbe44fb293
dmin3 <ekdrnzld@gmail.com>
2022-08-30 16:48:06 +0900

    22/8/30

---
+++ src/Number15657.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Number15657 {
+    static int n, m;
+    static int[] arr, result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        // 결과를 담아줄 배열
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 정렬
+        Arrays.sort(arr);
+
+        back(0, 0); // 백트래킹 실행
+
+        System.out.println(sb);
+    }
+
+    private static void back(int start, int depth) {
+        if (depth == m) {
+            for (int i : result) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            result[depth] = arr[i];
+            back(i, depth + 1);
+        }
+    }
+}
